2b65eb4df23d59dbc92adfbe016f2873cfef76f7
canurdikfit <161978264+canurdikfit@users.noreply.github.com>
2024-09-17 20:48:06 +0100

    Update MusicCarousel.tsx

--- src/components/ui/MusicCarousel.tsx
+++ src/components/ui/MusicCarousel.tsx
@@ -88,7 +88,7 @@ export default function MusicCarousel() {
 
 
     return (
-        <div className="flex flex-col items-center w-fit mx-auto md:items-end gap-3 max-h-[80vh]">
+        <div className="flex flex-col items-center w-fit mx-auto md:items-end gap-3 py-10">
             <div className='flex flex-col md:flex-row md:items-center'>
                 <div className="shrink-0 relative z-10 w-72 md:w-auto md:h-60 xl:h-64 h-80">
                     <img
